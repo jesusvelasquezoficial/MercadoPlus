2ccc148fdd5f19b3b5499db590356d38f2da82eb
John Doe <jesusvelasquezoficial@gmail.com>
2019-04-10 11:55:36 -0400

    Creado y modificado archivos de funciones Javascript

--- resources/head.php
+++ resources/head.php
@@ -3,22 +3,23 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="Información del promedio dolar oficial y paralelo en venezuela.">
 
-  <!-- Libs CSS -->
+  <!-- LIBS CSS -->
   <link rel="stylesheet" href="assets/fonts/feather/feather.min.css">
   <link rel="stylesheet" href="assets/libs/highlight.js/styles/vs2015.css">
   <link rel="stylesheet" href="assets/libs/quill/dist/quill.core.css">
   <link rel="stylesheet" href="assets/libs/select2/dist/css/select2.min.css">
   <link rel="stylesheet" href="assets/libs/flatpickr/dist/flatpickr.min.css">
-  <!-- Libs CSS CDN -->
+
+  <!-- LIBS CDN  -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
 
-  <!-- Theme CSS -->
+  <!-- THEME CSS -->
   <link rel="stylesheet" href="assets/css/theme.min.css" id="stylesheetLight">
   <link rel="stylesheet" href="assets/css/theme-dark.min.css" id="stylesheetDark">
 
   <!-- MAIN CSS -->
   <link rel="stylesheet" href="assets/css/main.css">
 
+  <!-- TITULO -->
   <title>Mercado Plus</title>
-
 </head>

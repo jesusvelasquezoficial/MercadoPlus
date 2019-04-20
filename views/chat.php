51e45c63b8243366b0f1499ec185c8bf33fc3505
Jesus Velasquez <jesusvelasquezoficial@gmail.com>
2019-04-20 16:48:35 -0400

    Creando pantilla de chat

--- views/chat.php
+++ views/chat.php
@@ -2,7 +2,7 @@
   <div class="$row">
     <div class="pt-6 h-auto" id="chat" style="height:100vh;">
       <form>
-        <div class="input-group input-group-lg input-group-flush input-group-merge>
+        <div class="input-group input-group-lg input-group-flush input-group-merge">
           <input type="text" class="form-control form-control-appended" placeholder="Escribe un mensaje aqui...">
           <div class="input-group-append">
             <div class="input-group-text">

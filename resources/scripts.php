4b945f5bdfddb13768bf6cbee454b14d38580337
John Doe <jesusvelasquezoficial@gmail.com>
2019-05-20 16:29:48 -0400

    Updated

--- resources/scripts.php
+++ resources/scripts.php
@@ -1,17 +1,33 @@
 <!-- JAVASCRIPT
 ================================================== -->
-<!-- LIBS JS -->
-<script src="assets/libs/jquery/dist/jquery.min.js"></script>
-<script src="assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+ <!-- LIBS JS PREDETERMINADAS
+ ==================================================================== -->
+
+ <script src="assets/libs/jquery/dist/jquery.min.js"></script>
+ <script src="assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+
+ <!-- ==================================================================== -->
+<!-- GRAFICOS -->
 <script src="assets/libs/chart.js/dist/Chart.min.js"></script>
-<script src="assets/libs/highlightjs/highlight.pack.min.js"></script>
-<script src="assets/libs/flatpickr/dist/flatpickr.min.js"></script>
-<script src="assets/libs/jquery-mask-plugin/dist/jquery.mask.min.js"></script>
-<script src="assets/libs/list.js/dist/list.min.js"></script>
-<script src="assets/libs/quill/dist/quill.min.js"></script>
-<script src="assets/libs/dropzone/dist/min/dropzone.min.js"></script>
-<script src="assets/libs/select2/dist/js/select2.min.js"></script>
+<!-- EXTENSION GRAFICOS -->
 <script src="assets/libs/chart.js/Chart.extension.min.js"></script>
+<!-- <script src="assets/libs/highlightjs/highlight.pack.min.js"></script> -->
+
+<!-- INPUTS CALENDARIO -->
+<script src="assets/libs/flatpickr/dist/flatpickr.min.js"></script>
+<!-- MASCARAS DE DATOS -->
+<script src="assets/libs/jquery-mask-plugin/dist/jquery.mask.min.js"></script>
+<!-- LISTAR DATOS -->
+<script src="assets/libs/list.js/dist/list.min.js"></script>
+<!-- EDITOR DE TEXTO -->
+<script src="assets/libs/quill/dist/quill.min.js"></script>
+<!-- EVALUAR ESTE RECURSO PARA MAS ADELANTE
+<script src="assets/libs/dropzone/dist/min/dropzone.min.js"></script> -->
+
+<!-- INPUTS SELECT -->
+<script src="assets/libs/select2/dist/js/select2.min.js"></script>
 
 <!-- CDN JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/push.js/1.0.9/push.js" integrity="sha256-p73+snCYhrDo9U6USD9P19LnY1EwAe4+VzQxj/kNyBE=" crossorigin="anonymous"></script>
